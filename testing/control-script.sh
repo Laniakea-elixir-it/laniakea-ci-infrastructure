1a6b4194181bed98417412a52aec165e663d7eab
mtangaro <ma.tangaro@gmail.com>
2021-12-07 14:56:38 +0100

    test

--- testing/control-script.sh
+++ testing/control-script.sh
@@ -4,6 +4,6 @@ export ORCHENT_AGENT_ACCOUNT=iam-recas-laniakea-test-jenkins
 export ORCHENT_URL="https://paas-orchestrator.cloud.ba.infn.it"
 
 /usr/bin/python --version
-
+echo $PWD
 # Run check script.
 python ./control-script.py -m laniakea.testuser@gmail.com -c "./health-check.sh" -u "https://paas-orchestrator.cloud.ba.infn.it" -r ./node_with_image.yaml
