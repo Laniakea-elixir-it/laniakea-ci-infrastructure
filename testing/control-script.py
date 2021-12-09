37ae31c4e6569ac21b28c792a0e53c74854db3f7
mtangaro <ma.tangaro@gmail.com>
2021-12-09 19:45:15 +0100

    fix port. email support to be removed and added to jenkins

--- testing/control-script.py
+++ testing/control-script.py
@@ -7,7 +7,21 @@ import time
 import logging
 import re
 
-logging.basicConfig(filename='/tmp/indigo_paas_checker.log', format='%(levelname)s %(asctime)s %(message)s', level=logging.DEBUG)
+#logging.basicConfig(filename='/tmp/indigo_paas_checker.log', format='%(levelname)s %(asctime)s %(message)s', level=logging.DEBUG)
+# Create logging facility print to stdout and to log file.
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# Create handler for log file and stdout
+logger_outfile_handler = logging.FileHandler('/tmp/indigo_paas_checker.log',mode='w')
+logger_stdout_handler = logging.StreamHandler(sys.stdout)
+# Set formatter for log file and stdout
+logger_formatter = logging.Formatter('%(levelname)s %(asctime)s %(message)s')
+logger_outfile_handler.setFormatter(logger_formatter)
+logger_stdout_handler.setFormatter(logger_formatter)
+# Load configuration for log file and stdout
+logger.addHandler(logger_outfile_handler)
+logger.addHandler(logger_stdout_handler)
+
 
 #______________________________________
 def cli_options():
@@ -15,7 +29,7 @@ def cli_options():
   parser = argparse.ArgumentParser(description='INDIGO PaaS checker status')
 
   parser.add_argument('-m', '--mail-address', dest='mail_address', help='Mail to send output')
-  parser.add_argument('-t', '--polling-timeout', dest='polling_time', default=300, help='Polling timeout')
+  parser.add_argument('-t', '--polling-timeout', dest='polling_time', default=30, help='Polling timeout') #default to 300
   parser.add_argument('-r', '--tosca-template', dest='tosca_template', help='TOSCA tempalte to be used')
   parser.add_argument('-c', '--healh_check_path', dest='health_check_path', help='Orchestrator health check script path')
   parser.add_argument('-u', '--orchestrator-url', dest='orchestrator_url', help='Orchestrator URL')
@@ -38,84 +52,87 @@ def check_orchestrator_status(path, url):
 
   command = '%s -u %s -t 10 -v' % (path, url)
 
-  print('Check Orchestrator status at: %s' % url)
+  logger.debug('Check Orchestrator status at: %s' % url)
 
   stdout, stderr, status = run_command(command)
 
-  print(stdout)
-  print(stderr)
+  logger.debug(stdout)
+  logger.debug(stderr)
 
   return status
 
 #______________________________________
 def depcreate(tosca, url):
 
-  command="/usr/bin/orchent depcreate %s '{}' -u %s" % (tosca, url)
-  logging.debug('%s' % command)
+  command="/usr/bin/orchent depcreate " + tosca + " '{}' -u " + url
+  logger.debug(command)
   
   stdout, stderr, status = run_command(command)
-  temp = stdout.split("Deployment",1)[1]
-  
-  dep_uuid = temp.splitlines()[0].strip(" []:")
-  logging.debug('[Deployment] Uuid: %s'% dep_uuid)
-  
-  dep_status = temp.splitlines()[1].strip(" status: ")
-  logging.debug('[Deployment] Uuid: %s, status: %s' % (dep_uuid, dep_status))
 
-  return dep_uuid, dep_status
+  temp = stdout.split(b"Deployment",1)[1]
+
+  dep_uuid = temp.splitlines()[0].strip(b" []:")
+  logger.debug('[Deployment] Uuid: %s'% dep_uuid)
+  
+  dep_status = temp.splitlines()[1].strip(b" status: ")
+  logger.debug('[Deployment] Uuid: %s, status: %s' % (dep_uuid, dep_status))
+
+  return dep_uuid.decode("utf-8"), dep_status.decode("utf-8")
 
 #______________________________________
 def depshow(uuid):
 
-  command="/usr/bin/orchent depshow %s" % uuid
+  command="/usr/bin/orchent depshow " + uuid
 
   stdout, stderr, status = run_command(command)
 
-  return stdout, stderr, status
+  return stdout.decode("utf-8"), stderr.decode("utf-8"), status.decode("utf-8")
 
 #______________________________________
 def depdel(uuid):
 
-  command="/usr/bin/orchent depdel %s" % uuid
+  command="/usr/bin/orchent depdel " + uuid
   
   stdout, stderr, status = run_command(command)
   
-  return stdout, stderr, status
+  return stdout.decode("utf-8"), stderr.decode("utf-8"), status.decode("utf-8")
 
 #______________________________________
 def get_status(dep_uuid):
-  
-  command="/usr/bin/orchent depshow %s" % dep_uuid
+ 
+# qui c'è un problema. anche se il deploy non viene cancellato viene mostrato come "already" deleted.
+
+  command="/usr/bin/orchent depshow " + dep_uuid
 
   stdout, stderr, status = run_command(command)
 
   # check if the deployment has been deleted
   #pattern = "Error 'Not Found' [404]"
   pattern = "doesn't exist"
-  match = re.search(pattern, stdout)
+  match = re.search(pattern, stdout.decode("utf-8"))
   if (match is not None): 
-    logging.debug('Deployment with uuid %s already deleted!' % dep_uuid)
+    logger.debug('Deployment with uuid %s already deleted!' % dep_uuid)
     return 'DELETE_COMPLETE'
 
-  temp = stdout.split("Deployment",1)[1]
+  temp = stdout.split(b"Deployment",1)[1]
 
-  dep_status = temp.splitlines()[1].strip(" status: ")
+  dep_status = temp.splitlines()[1].strip(b" status: ")
 
-  return dep_status
+  return dep_status.decode("utf-8")
 
 #______________________________________
 def start():
 
-  logging.debug('======================================')
-  logging.debug('Start new check!')
-  logging.debug('======================================')
+  logger.debug('======================================')
+  logger.debug('Start new check!')
+  logger.debug('======================================')
 
 #______________________________________
 def end():
 
-  logging.debug('**************************************')
-  logging.debug('Check ended!')
-  logging.debug('**************************************')
+  logger.debug('**************************************')
+  logger.debug('Check ended!')
+  logger.debug('**************************************')
 
 
 #______________________________________
@@ -131,9 +148,8 @@ def indigo_paas_checker():
 
   # Check orchestrator status.
   orchestrator_status = check_orchestrator_status(options.health_check_path, options.orchestrator_url)
-
   if(orchestrator_status is not 0):
-    print('Unable to contact the orchestrator at %s.' % options.orchestrator_url)
+    logger.debug('Unable to contact the orchestrator at %s.' % options.orchestrator_url)
     end()
     sys.exit(1)
 
@@ -149,40 +165,40 @@ def indigo_paas_checker():
     time.sleep(float(options.polling_time))
     dep_status = get_status(dep_uuid)
     count = count + 1
-    logging.debug('[Deployment] Update n. %s, uuid: %s, status: %s.' % (count, dep_uuid, dep_status))
-  logging.debug('Deployment uuid %s finished with status: %s' % (dep_uuid, dep_status))
+    logger.debug('[Deployment] Update n. %s, uuid: %s, status: %s.' % (count, dep_uuid, dep_status))
+  logger.debug('Deployment uuid %s finished with status: %s' % (dep_uuid, dep_status))
 
   # wait some secs.
   #time.sleep(10)
 
-  # Send report if create failed
-  if(dep_status == 'CREATE_FAILED'):
-    final_out, final_err, final_status = depshow(dep_uuid)
-    sendmail = 'echo "Deployment uuid: %s \n \n  %s" | /usr/bin/mail -s "[INDIGO PaaS] check at: $(date)" %s' % (dep_uuid, final_out, options.mail_address)
-    run_command(sendmail)
-    logging.debug('Report sent to: %s' % options.mail_address) 
+  ## Send report if create failed
+  #if(dep_status == 'CREATE_FAILED'):
+  #  final_out, final_err, final_status = depshow(dep_uuid)
+  #  sendmail = 'echo "Deployment uuid: %s \n \n  %s" | /usr/bin/mail -s "[INDIGO PaaS] check at: $(date)" %s' % (dep_uuid, final_out, options.mail_address)
+  #  run_command(sendmail)
+  #  logger.debug('Report sent to: %s' % options.mail_address) 
 
-  # Always delete deployment
+  ## Always delete deployment
   final_out, final_err, final_status = depdel(dep_uuid)  
 
-  # Ensure deletion
-  # This should get rid also of concurrency.
+  ## Ensure deletion
+  ## This should get rid also of concurrency.
   del_count = 1 # delete already triggered 1 time
   pattern='successfully triggered'
-  match = re.search(pattern, final_out)
+  match = re.search(pattern, str(final_out))
   while(match is None): 
-    logging.debug('Deployment uuid %s delete failed. Wait for 2 minutes and retry.' % dep_uuid)
+    logger.debug('Deployment uuid %s delete failed. Wait for 2 minutes and retry.' % dep_uuid)
     time.sleep(120)
     del_count = del_count + 1
-    logging.debug('Delete uuid %s retry n. %s.' % (dep_uuid, del_count))
+    logger.debug('Delete uuid %s retry n. %s.' % (dep_uuid, del_count))
     final_out, final_err, final_status = depdel(dep_uuid)
-    match = re.search(pattern, repl)  
-  logging.debug('Deployment uuid %s delete: %s' % (dep_uuid, final_out))
+    match = re.search(pattern, final_out)
+  logger.debug('Deployment uuid %s delete: %s' % (dep_uuid, final_out))
 
-  # Notify delete failed.
-  # Check deployment status during delete in progress
-  dep_status = get_status(dep_uuid)
-  logging.debug('[Deletion] Deployment uuid %s status: %s' % (dep_uuid, dep_status))
+  ## Notify delete failed.
+  ## Check deployment status during delete in progress
+  #dep_status = get_status(dep_uuid)
+  #logger.debug('[Deletion] Deployment uuid %s status: %s' % (dep_uuid, dep_status))
 
   # reset counter
   count = 0
@@ -190,18 +206,17 @@ def indigo_paas_checker():
     time.sleep(60)
     dep_status = get_status(dep_uuid)
     count = count + 1
-    logging.debug('[Deletion] Update n. %s, uuid %s, status: %s.' % (count, dep_uuid, dep_status))
-  logging.debug('Delete finished.')
+    logger.debug('[Deletion] Update n. %s, uuid %s, status: %s.' % (count, dep_uuid, dep_status))
+  logger.debug('Delete finished.')
 
-  # Send report if create failed
-  # Update deployment status
-  dep_status = get_status(dep_uuid)
-  if(dep_status == 'DELETE_FAILED'):
-    final_out, final_err, final_status = depshow(dep_uuid)
-    sendmail = 'echo "Deployment delete failed uuid: %s \n  %s" | /usr/bin/mail -s "[INDIGO PaaS] check at: $(date)" %s' % (dep_uuid, final_out, options.mail_address)
-    run_command(sendmail)
-    logging.debug('Report sent to: %s' % options.mail_address)
-
+  ## Send report if create failed
+  ## Update deployment status
+  #dep_status = get_status(dep_uuid)
+  #if(dep_status == 'DELETE_FAILED'):
+  #  final_out, final_err, final_status = depshow(dep_uuid)
+  #  sendmail = 'echo "Deployment delete failed uuid: %s \n  %s" | /usr/bin/mail -s "[INDIGO PaaS] check at: $(date)" %s' % (dep_uuid, final_out, options.mail_address)
+  #  run_command(sendmail)
+  #  logger.debug('Report sent to: %s' % options.mail_address)
 
   end()
 
