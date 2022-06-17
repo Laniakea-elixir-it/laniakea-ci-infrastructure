ef896b686c68c09c6dea096293239f28e05a24cb
mtangaro <ma.tangaro@gmail.com>
2022-06-17 15:31:53 +0000

    fix log

--- cloud-images/recas_centos7_2nic_config.sh
+++ cloud-images/recas_centos7_2nic_config.sh
@@ -6,3 +6,7 @@ LOGFILE='/var/log/recas-netconfig.log'
 touch $LOGFILE
 
 echo 'Test cloud init' > $LOGFILE
+
+
+cat /etc/sysconfig/network-scripts/ifcfg-eth0 >> $LOGFILE
+cat /etc/sysconfig/network-scripts/ifcfg-eth1 >> $LOGFILE
