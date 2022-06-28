9a3d78a1913bf8dcada5f7bd141121cbb5725822
Daniele <daniele.colombo39@gmail.com>
2022-06-28 10:47:54 +0200

    Remove oidc-add force

--- testing/expect_laniakea_dev.sh
+++ testing/expect_laniakea_dev.sh
@@ -2,7 +2,7 @@
 exp_internal 1
 set timeout -1
 set secret [lindex $argv 0]
-spawn oidc-add -f iam-recas-test_laniakea-dev_matangaro_jenkins-test
+spawn oidc-add iam-recas-test_laniakea-dev_matangaro_jenkins-test
 match_max 100000
 expect -exact "\[36mEnter decryption password for account config 'iam-recas-test_laniakea-dev_matangaro_jenkins-test'\[0m\[36m: \[0m"
 send -- "$secret\r"
