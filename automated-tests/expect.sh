07870310434b9613d3ea85abe7bda222728546c0
mtangaro <ma.tangaro@gmail.com>
2024-07-09 13:11:04 +0000

    update iam recas iam client

--- automated-tests/expect.sh
+++ automated-tests/expect.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect -f
 set timeout -1
 set secret [lindex $argv 0]
-spawn oidc-add iam-recas-laniakea-test-jenkins
+spawn oidc-add iam-recas-laniakea-bot
 match_max 100000
-expect -exact "\[36mEnter decryption password for account config 'iam-recas-laniakea-test-jenkins'\[0m\[36m: \[0m"
+expect -exact "\[36mEnter decryption password for account config 'iam-recas-laniakea-bot'\[0m\[36m: \[0m"
 send -- "$secret\r"
 expect eof
