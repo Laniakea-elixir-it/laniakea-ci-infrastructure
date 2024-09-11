f07d3d1a467d40ca51ce33aecf363e4d143e20b3
mtangaro <ma.tangaro@gmail.com>
2024-09-11 10:50:38 +0000

    try to fix delete options

--- automated-tests/control-script.py
+++ automated-tests/control-script.py
@@ -112,7 +112,7 @@ def run_test_list(test_list, group, orchestrator_url, polling_time):
           run_additional_tests(dep, run_more)
       # Delete deployment even if tests failed 
       finally:
-          delete = str(test_list['test'][i]['delete'])
+          delete = test_list['test'][i]['delete']
           test_exit_status = end_test(dep, create_status_record, delete)
 
       if test_exit_status:
