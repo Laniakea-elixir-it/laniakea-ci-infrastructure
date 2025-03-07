6f48b160ef92dc9e05a0ca8694c0bac6ae8b18c5
mtangaro <ma.tangaro@gmail.com>
2025-03-07 18:45:34 +0000

    add debug

--- cloud-images/build_images.py
+++ cloud-images/build_images.py
@@ -167,7 +167,7 @@ def build_image(path):
     """
     Run subprocess call redirecting stdout, stderr and the command exit code.
     """
-    cmd = packer_exe + ' build ' + path
+    cmd = packer_exe + ' build ' + path + '-debug'
     logger.debug(cmd)
 
     proc = subprocess.Popen( args=cmd, shell=True,  stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
